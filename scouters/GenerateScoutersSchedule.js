47fd10fadb3d29f696e262a517d39a8484d38837
barryb5 <bbalasingham@gmail.com>
2023-01-05 19:44:54 -0800

    [GenerateScoutersSchedule] Added match number back to scoutersSchedule. It now works with non-qual matches and is spread out properly for the double elimination format

--- scouters/GenerateScoutersSchedule.js
+++ scouters/GenerateScoutersSchedule.js
@@ -8,7 +8,7 @@ const availability = Papa.parse(fs.readFileSync('./scouters/Availability.csv', '
 let version = 1
 let previous = fs.readFileSync(`./scouters/scoutersSchedule.json`, 'utf8')
 if (undefined != previous && previous.includes('version')) {
-    console.log(`exists`)
+    console.log(`Previous version already exists`)
     version = JSON.parse(previous).version + 1
 }
 
@@ -41,26 +41,45 @@ run = async () => {
         let startMatchKey = tournamentKey + '_'
         let endMatchKey = tournamentKey + '_'
 
+        let startMatchNumber = 0
+        let endMatchNumber = 0
+
         if ((i*shiftSize + 1) < amountOfQms && (i*shiftSize + shiftSize) < amountOfQms) {
             // Before amount of qms
+            startMatchNumber = (i*shiftSize + 1)
+            endMatchNumber = (i*shiftSize + shiftSize)
             startMatchKey += "qm" + (i*shiftSize + 1)
             endMatchKey += "qm" + (i*shiftSize + shiftSize)
         } else if ((i*shiftSize + 1) < amountOfQms) {
             // Stops at amount of qms because after would be pick
+            startMatchNumber = (i*shiftSize + 1)
+            endMatchNumber = amountOfQms
             startMatchKey += "qm" + (i*shiftSize + 1)
             endMatchKey += "qm" + amountOfQms
         } else {
             if (nonqual == 0) {
                 // First non qual match
+                startMatchNumber = amountOfQms + 1
+                endMatchNumber = amountOfQms + 4
                 startMatchKey += "qf0"
                 endMatchKey += "qf3"
             } else if (nonqual == 1) {
+                // Figured out startMatchNumber and endMatchNumber like this because it's easier to keep track of how many matches in a non-qual shift (since double elimination)
+                startMatchNumber = amountOfQms + 4 + 1
+                endMatchNumber = amountOfQms + 4 + 4
                 startMatchKey += "ef0"
                 endMatchKey += "sf1"
             } else if (nonqual == 2) {
+                // Winners finals happens between ef2 and ef5
+                startMatchNumber = amountOfQms + 4 + 4 + 1
+                endMatchNumber = amountOfQms + 4 + 4 + 5
                 startMatchKey += "ef2"
+                // ef5 is also known as Losers Finals, but isn't labelled differently ig
                 endMatchKey += "ef5"
             } else if (nonqual == 3) {
+                // Grand Finals
+                startMatchNumber = amountOfQms + 4 + 4 + 5 + 1
+                endMatchNumber = amountOfQms + 4 + 4 + 5 + 3
                 startMatchKey += "gf0"
                 endMatchKey += "gf2"
             } else {
@@ -72,8 +91,10 @@ run = async () => {
         }
 
         newData.shifts[i] = {
-            'start': startMatchKey,
-            'end': endMatchKey,
+            'start': startMatchNumber,
+            'end': endMatchNumber,
+            'startKey': startMatchKey,
+            'endKey': endMatchKey,
             'scouts': []
         }
 
