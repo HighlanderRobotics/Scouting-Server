d12399b45b019305d9358779f9cf96f4099c16dd
barryb5 <bbalasingham@gmail.com>
2023-01-06 19:40:13 -0800

    [GetMatches] Mostly works, however ef6 and f2 share the same match number because f2 is supposed to be gf2 in tba api

--- addTestData.js
+++ addTestData.js
@@ -2,7 +2,7 @@ const sqlite = require('sqlite3').verbose()
 const db = new sqlite.Database('./test.db', sqlite.OPEN_READWRITE, (err) => {
   if (err)
       console.error(err)
-});
+})
 require('dotenv').config()
 const DatabaseManager = require('./DatabaseManager.js')
 
