f1ef45e144ea7920f6c0a551669e4724b3ebdf80
barryb5 <bbalasingham@gmail.com>
2023-01-12 14:02:21 -0800

    [Copied] Copied a commit over and stopped it from crashing on startup

--- TaskManager.js
+++ TaskManager.js
@@ -3,15 +3,16 @@ const Manager = require('./manager/dbmanager.js')
 // const AverageForMetric = require('./analysis/AverageForMetric.js')
 //const Overview = require('./overview.js')
 
-
 const fullyScouted = require('./analysis/general/fullyScouted.js')
-const defenseOverview = require('./analysis/defense/defenseOverview.js')
-const breakdownMetrics = require('./analysis/breakdownMetrics.js')
-const categoryMetrics = require('./analysis/categoryMetrics.js')
-const cargoCountOverview = require('./analysis/teleop/cargo/cargoOverview.js')
-const cargoCountAutoOverview = require('./analysis/auto/cargo/cargoAutoOverview.js')
-const cyclingOverview = require('./analysis/teleop/cargo/cyclingOverview.js')
 
+const notes = require('./analysis/general/notes.js')
+const scores = require('./analysis/general/averageScore.js')
+// const cyclingOverview = require('./analysis/teleop/cargo/cyclingOverview.js')
+// const cargoCountAutoOverview = require('./analysis/auto/cargo/cargoCountAutoOverview.js')
+// const cargoCountOverview = require('./analysis/teleop/cargo/cargoCountOverview.js')
+// const categoryMetrics = require('./analysis/catergoryMetrics.js')
+// const breakdown = require('./analysis/breakdown.js')
+// const defenseOverview = require('./analysis/defense/defenseOverview.js')
 
 class TaskManager {
 
@@ -66,32 +67,24 @@ class TaskManager {
             switch (task.name) {
                 case(cyclingOverview.name):
                     returnAnalysis.push(new cyclingOverview(Manager.db, task.team, task.type, task.location))
-                case(cyclingOverview.name):
-                    returnAnalysis.push(new cyclingOverview(Manager.db, task.team, task.type, task.location))
                     break
+                case(cargoCountAutoOverview.name):
+                    returnAnalysis.push(new cargoCountAutoOverview(Manager.db, task.team, task.type))
                 case(cargoCountAutoOverview.name):
                     returnAnalysis.push(new cargoCountAutoOverview(Manager.db, task.team, task.type))
                     break
                 case(cargoCountOverview.name):
                     returnAnalysis.push(new cargoCountOverview(Manager.db, task.team, task.type))
-                case(cargoCountOverview.name):
-                    returnAnalysis.push(new cargoCountOverview(Manager.db, task.team, task.type))
                     break
                 case(categoryMetrics.name):
                     returnAnalysis.push(new categoryMetrics(Manager.db, task.team))
-                case(categoryMetrics.name):
-                    returnAnalysis.push(new categoryMetrics(Manager.db, task.team))
                     break
-                case(breakdownMetrics.name):
+                case(breakdown.name):
                     returnAnalysis.push(new breakdown(Manager.db, task.team))
                     break
                 case(defenseOverview.name):
                     returnAnalysis.push(new defenseOverview(Manager.db, task.team, task.type))
-                case(defenseOverview.name):
-                    returnAnalysis.push(new defenseOverview(Manager.db, task.team, task.type))
                     break
-                case(fullyScouted.name):
-                    returnAnalysis.push(new fullyScouted(Manager.db, task.team))
                 case(robotRole.name):
                     returnAnalysis.push(new robotRole(Manager.db, task.team))
                     break
