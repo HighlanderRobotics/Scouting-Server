ec89acd846665be2f4026a04ab5b6633704b6eca
barryb5 <bbalasingham@gmail.com>
2023-01-14 21:19:48 -0800

    [Access-Control-Allow-Origin] Should work now followed instructions on :alien:

--- collectionServer.js
+++ collectionServer.js
@@ -52,9 +52,6 @@ setup = async () => {
         console.log(`\nngrok link is not setup/running\n`)
     }
 
-
-    
-
     return url
 }
 
@@ -83,9 +80,39 @@ app.use(morgan('readable', {
     stream: writer
 }))
 
+// More middleware to allow Access-Control-Allow-Origin
+app.use(async (req, res, next) => {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4000')
+
+    try {
+        url = await axios.get('http://localhost:4040/api/tunnels')
+        .then((res) => {
+            return res.data.tunnels[0].public_url
+        })
+
+        res.setHeader('Access-Control-Allow-Origin', url)
+    } catch (e) {
+        
+    }
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true)
+
+    // Pass to next layer of middleware
+    next()
+})
+
 // Temporary if others want to use old endpoints for integration test day, will force changing endpoints later
 const Manager = require('./manager/dbmanager.js')
-const { json } = require('body-parser')
 
 // Tasks map
 const uuidToTask = new Map()
