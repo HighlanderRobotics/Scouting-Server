380f439be4fe479f49bbe5cba07b4e98fdd53b14
barryb5 <bbalasingham@gmail.com>
2023-01-15 11:40:43 -0800

    [CORS] Cors can go die aswell

--- collectionServer.js
+++ collectionServer.js
@@ -81,35 +81,12 @@ app.use(morgan('readable', {
 }))
 
 // More middleware to allow Access-Control-Allow-Origin
-app.use(async (req, res, next) => {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4000')
-
-    try {
-        url = await axios.get('http://localhost:4040/api/tunnels')
-        .then((res) => {
-            return res.data.tunnels[0].public_url
-        })
-
-        res.setHeader('Access-Control-Allow-Origin', url)
-    } catch (e) {
-        
-    }
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true)
-
-    // Pass to next layer of middleware
-    next()
-})
+const cors = require('cors')
+app.use(cors({
+    origin: true
+}))
+// app.options('*', cors())
+// app.options('/API/manager/:task', cors())
 
 // Temporary if others want to use old endpoints for integration test day, will force changing endpoints later
 const Manager = require('./manager/dbmanager.js')
