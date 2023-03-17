877205f3ead0248a901bb3087fd9ee462757a7de
CassieColby <70112547+CassieColby@users.noreply.github.com>
2023-03-16 21:28:13 -0700

    tournaments ket and name endpoints

--- DatabaseManager.js
+++ DatabaseManager.js
@@ -17,6 +17,7 @@ const GetAllNotes = require('./manager/GetAllNotes.js')
 const NewScouter = require('./manager/NewScouter.js')
 const MatchesCompleted = require('./manager/MatchesCompleted.js')
 const GetTeamsInTournament = require('./manager/GetTeamsInTournament.js')
+const GetTournamentKey = require('./manager/GetTournamentKey.js')
 
 class DatabaseManager {
     constructor() {
@@ -63,6 +64,8 @@ class DatabaseManager {
                     return new MatchesCompleted().runTask(body)
                 case GetTeamsInTournament.name:
                     return new GetTeamsInTournament().runTask(body.tournamentKey)
+                case GetTournamentKey.name:
+                    return new GetTournamentKey().runTask()
                 default:
                     return new Promise((resolve, reject) => {
                         reject({
--- test.js
+++ test.js
@@ -1,14 +1,16 @@
 //for testing
 // const test = require('./analysis/general/scoringBreakdown')
-const test = require('./analysis/teleop/cargo/cargoCountOverview')
+// const test = require('./analysis/teleop/cargo/cargoCountOverview')
+const test = require('./manager/GetTournamentKey')
 const Manager = require('./manager/dbmanager')
 
 // const y = require("./test")
 async function temp() {
    // var x = new test(Manager.db, "2023cafr", 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5)
-   var x = new test(Manager.db, 8033, 0, 2)
-  await x.runAnalysis()
- console.log(x.finalizeResults())
+   // var x = new test(Manager.db, 8033, 0, 2)
+//   await x.runAnalysis()
+   let x = await new test().runTask()
+ console.log(x)
 }
 temp()
 
