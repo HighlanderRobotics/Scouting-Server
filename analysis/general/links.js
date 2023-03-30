4f557dd20a03348a97fa49fe9caeb4b42a8eb0f2
CassieColby <70112547+CassieColby@users.noreply.github.com>
2023-03-30 01:41:47 -0700

    links fixed

--- analysis/general/links.js
+++ analysis/general/links.js
@@ -1,4 +1,4 @@
-const { boolean, to } = require('mathjs')
+const { boolean, to, count, resolve } = require('mathjs')
 const BaseAnalysis = require('../BaseAnalysis.js')
 // const Manager = require('./manager/dbmanager.js')
 
@@ -12,93 +12,78 @@ class links extends BaseAnalysis {
 
     }
     async getAccuracy() {
-
         let a = this
         let arr = []
         let matches = []
-        return new Promise(async function (resolve, reject) {
-            let sql = `SELECT newMatches.key, newMatches.tournamentKey, newMatches.matchNumber, newMatches.matchType, data.scoutReport
-             FROM data
-             JOIN (SELECT matches.key AS key, matches.tournamentKey, matches.matchNumber, matches.matchType
-                FROM matches 
-                JOIN teams ON teams.key = matches.teamKey
-                WHERE teams.teamNumber = ?) AS newMatches ON data.matchKey = newMatches.key`
-           let sql2 = `SELECT scoutReport, newMatches.key AS key
-                FROM data
-                JOIN (SELECT matches.key AS key
-                    FROM matches
-                    WHERE matches.matchNumber = ? )
-                   AS newMatches ON data.matchKey = newMatches.key `
-                a.db.all(sql, [a.team], (err, rows) => {
+        let sql = `SELECT newMatches.key, newMatches.tournamentKey, newMatches.matchNumber, newMatches.matchType, data.scoutReport
+                   FROM data
+                   JOIN (SELECT matches.key AS key, matches.tournamentKey, matches.matchNumber, matches.matchType
+                      FROM matches 
+                      JOIN teams ON teams.key = matches.teamKey
+                      WHERE teams.teamNumber = ?) AS newMatches ON data.matchKey = newMatches.key`
+        let sql2 = `SELECT scoutReport, newMatches.key AS key
+                    FROM data
+                    JOIN (SELECT matches.key AS key
+                          FROM matches
+                          WHERE matches.matchNumber = ? )
+                         AS newMatches ON data.matchKey = newMatches.key `
+        let rows = await new Promise((resolve, reject) => {
+            a.db.all(sql, [a.team], (err, rows) => {
                 if (err) {
                     console.log(err)
                     reject(err)
                 }
-                for (let i = 0; i < rows.length; i++) {
-                    a.db.all(sql2, [rows[i].matchNumber], (err, rows2) => {
-                        if (err) {
-                            console.log(err)
-                            reject(err)
-                        }
-                        let total = 0
-                        let low = false
-                        let currKey = rows[i].key
-                        if (currKey.substring(rows[i].key.length - 1) <= 2) {
-                            low = true
-                        }
-                        let teamTotal = 0
-                        let parsedTeam = JSON.parse(rows[i].scoutReport).events
-                        for (let z = 0; z < parsedTeam.length; z++)
-                        {
-                            if(parsedTeam[z][1] === 2)
-                            {
-                                teamTotal += 1
-                            }
-                        }
-                        for (let j = 0; j < rows2.length; j++) {
-                            if (rows2[j].key.substring(rows2[j].key.length - 1) <= 2 == low) {
-
-                                let curr = JSON.parse(rows2[j].scoutReport).events
-                                for (let x = 0; x < curr.length; x++) {
-
-                                    if (curr[x][1] === 2) {
-                                        total += 1
-                                    }
-                                }
-                            }
-
-                        }
-                        let muilt = teamTotal/total
-                        matches.push(rows[i].key)
-                        arr.push(rows[i].links * muilt)
-
-                    })
-
-                }
-                a.array = arr.map((item, index) => ({
-                    "match": matches[index],
-                    "value": item,
-                }))
-                a.result =  arr.reduce((partialSum, a) => partialSum + a, 0) / arr.length
-                if(isNaN(a.result))
-                {
-                    a.result = 0
-                }
-                resolve("done")
-
+                resolve(rows)
             })
-
         })
-            .catch((err) => {
-                if (err) {
-                    return err
+        await Promise.all(rows.map(async (row) => {
+            let rows2 = await new Promise((resolve, reject) => {
+                a.db.all(sql2, [row.matchNumber], (err, rows2) => {
+                    if (err) {
+                        console.log(err)
+                        reject(err)
+                    }
+                    resolve(rows2)
+                })
+            })
+            let total = 0
+            let low = false
+            let currKey = row.key
+            if (currKey.substring(row.key.length - 1) <= 2) {
+                low = true
+            }
+            let teamTotal = 0
+            let parsedTeam = JSON.parse(row.scoutReport).events
+            for (let z = 0; z < parsedTeam.length; z++) {
+                if (parsedTeam[z][1] === 2) {
+                    teamTotal += 1
                 }
-            })
-            .then((data) => {
-                // console.log(data)
-                return data
-            })
+            }
+            for (let j = 0; j < rows2.length; j++) {
+                if (rows2[j].key.substring(rows2[j].key.length - 1) <= 2 == low) {
+                    let curr = JSON.parse(rows2[j].scoutReport).events
+                    for (let x = 0; x < curr.length; x++) {
+                        if (curr[x][1] === 2) {
+                            total += 1
+                        }
+                    }
+                }
+            }
+            let muilt = teamTotal / total
+            arr.push(JSON.parse(row.scoutReport).links * muilt)
+            matches.push(row.key)
+        }))
+        a.array = arr.map((item, index) => ({
+            "match": matches[index],
+            "value": item,
+        }))
+        a.result = arr.reduce((partialSum, a) => partialSum + a, 0) / arr.length
+        if (isNaN(a.result)) {
+            a.result = 0
+        }
+        resolve("done")
     }
+
 
     runAnalysis() {
 
@@ -118,8 +103,8 @@ class links extends BaseAnalysis {
     finalizeResults() {
         return {
             "result": this.result,
-            "array" : this.array,
-            "team" : this.team
+            "array": this.array,
+            "team": this.team
         }
     }
 
