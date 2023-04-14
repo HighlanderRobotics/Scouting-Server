cf6a7cdc80fb1cd268a4ca9484bd4f5fc5dde5eb
CassieColby <70112547+CassieColby@users.noreply.github.com>
2023-04-14 11:39:31 -0700

    fixed setup

--- manager/getPickedTeams.js
+++ manager/getPickedTeams.js
@@ -1,33 +1,33 @@
-const Manager = require('./Manager.js')
-const { Server } = require("socket.io")
-const { resolve } = require('mathjs')
-const server = require("../collectionServer.js")
-const io = new Server(server)
+// const Manager = require('./Manager.js')
+// const { Server } = require("socket.io")
+// const { resolve } = require('mathjs')
+// const server = require("../collectionServer.js")
+// const io = new Server(server)
 
 
-class getPickedTeams extends Manager {
-    static name = "getPickedTeams"
+// class getPickedTeams extends Manager {
+//     static name = "getPickedTeams"
 
-    constructor() {
-        super()
-    }
+//     constructor() {
+//         super()
+//     }
 
-    async runTask(){
+//     async runTask(){
 
-        var sql = `SELECT *
-        FROM pickedTeams`
-        return new Promise(async (resolve, reject) => {
-            Manager.db.all(sql, [], (err, rows) => {
-                if (err) {
-                    console.log(err)
-                    reject(err)
-                }
-                io.emit('pickedTeams', rows)
-                resolve(rows)
-            })
-        })
+//         var sql = `SELECT *
+//         FROM pickedTeams`
+//         return new Promise(async (resolve, reject) => {
+//             Manager.db.all(sql, [], (err, rows) => {
+//                 if (err) {
+//                     console.log(err)
+//                     reject(err)
+//                 }
+//                 io.emit('pickedTeams', rows)
+//                 resolve(rows)
+//             })
+//         })
 
-    }
-}
+//     }
+// }
 
-module.exports = getPickedTeams
+// module.exports = getPickedTeams
