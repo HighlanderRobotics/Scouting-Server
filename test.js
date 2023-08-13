727fa3699a8b956994cfb8e852a8a9f6972e8eff
CassieColby <70112547+CassieColby@users.noreply.github.com>
2023-08-12 22:20:27 -0700

    picklist not crashing when no data

--- test.js
+++ test.js
@@ -1,15 +1,15 @@
 //for testing
-const test = require('./manager/GetMatches')
+const test = require('./analysis/picklistShell')
 
 const Manager = require('./manager/dbmanager')
 
 // const y = require("./test")
 async function temp() {
-   // var x = new test(Manager.db, "2023gal", 0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+   var x = new test(Manager.db, "2023gal", 0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
    // let x = new test(Manager.db, 8033)
    // await x.runAnalysis()
-   let temp = await new test().runTask({tournamentKey :"2023gal"})
-   console.log(temp)
+   // let temp = await new test().runTask({tournamentKey :"2023gal"})
+   console.log(   await x.runAnalysis()   )
 }
 temp()
 
