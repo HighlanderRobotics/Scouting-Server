b1c339c2b841c6915965f61e904c4291637ed861
CassieColby <70112547+CassieColby@users.noreply.github.com>
2023-08-28 19:10:07 -0700

    trend file added

---
+++ analysis/trend.js
@@ -0,0 +1,82 @@
+const BaseAnalysis = require('./BaseAnalysis.js')
+const AverageScore = require('./general/averageScore.js')
+const math = require('mathjs')
+
+class trends extends BaseAnalysis {
+    static name = `trends`
+
+    constructor(db, team) {
+        super(db)
+        this.team = team
+        this.result = this.result
+    }
+    async getTrend() {
+        let a = this
+        let score = new AverageScore(a.db, a.team, 1)
+        await score.runAnalysis()
+        let scoreArr = score.array
+        let avgScore = score.average
+        if (scoreArr.length <= 2)
+        {
+            a.result = 2
+        }
+        else{
+            let num = (scoreArr[scoreArr.length-3] - scoreArr[scoreArr.length-2]) + (scoreArr[scoreArr.length-2] - scoreArr[scoreArr.length-1])/2
+            if(math.abs(num) >= avgScore/3)
+            {
+                if(num < 0)
+                {
+                    a.result = 0
+                }
+                else
+                {
+                    a.result = 4
+                }
+            }
+            else if (math.abs(num) <= avgScore/10 )
+            {
+                
+
+                a.result = 2
+            }
+            else
+            {
+
+                if(num < 0)
+                {
+                    a.result = 1
+                }
+                else
+                {
+                    a.result = 3
+                }
+            }
+        }
+            
+        resolve("done")
+
+    }
+    
+
+   
+    runAnalysis() {
+        let a = this
+        return new Promise(async (resolve, reject) => {
+            await a.getTrend().catch((err) => {
+
+            })
+            resolve("done")
+        })
+
+
+    }
+    
+    finalizeResults() {
+        return {
+            "team" : this.team,
+            "result" : this.result
+        }
+    }
+}
+    
+module.exports = trends
--- test.js
+++ test.js
@@ -1,15 +1,13 @@
 //for testing
-const test = require('./analysis/teamAndMatch')
+const test = require('./analysis/trend')
 
 const Manager = require('./manager/dbmanager')
 
 // const y = require("./test")
 async function temp() {
-   // var x = new test(Manager.db, "2023gal", 0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
-   let x = new test(Manager.db, 8033, '2023gal_qm87_4')
+   let x = new test(Manager.db, 1678)
    await x.runAnalysis()
-  //  let temp = await new test().runTask({tournamentKey :"2023gal"})
-   console.log(x.finalizeResults().result)
+   console.log(x.finalizeResults())
 }
 temp()
 
